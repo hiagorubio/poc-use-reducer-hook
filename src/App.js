d5abf15367743be4b834c0d6ea07cdf8cacd0520
Hiago Rubio <hiago.rubio@minutrade.com>
2020-03-05 11:26:08 -0300

    SBI-219 Studing useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment': return state + 1;
+    case 'decrement': return state - 1;
+    case 'incrementTen': return state + 10;
+    case 'decrementTen': return state - 10;
+    case 'reset': return 0;
+    default: throw new Error('Unexpected action');
+  }
+};
+
+const ButtonWithReducer = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <>
+      <div className="item">
+        {count}
+      </div>
+      <div className="App">
+        <div className="item">
+          <button onClick={() => dispatch('increment')}>+1</button>
+        </div>
+        <div className="item">
+          <button onClick={() => dispatch('decrement')}>-1</button>
+        </div>
+        <div className="item">
+          <button onClick={() => dispatch('incrementTen')}>+10</button>
+        </div>
+        <div className="item">
+          <button onClick={() => dispatch('decrementTen')}>-10</button>
+        </div>
+        <div className="item">
+          <button className="resetBtn" onClick={() => dispatch('reset')}>reset</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonWithReducer />
     </div>
   );
 }
